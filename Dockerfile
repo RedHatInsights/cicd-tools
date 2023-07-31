8e0db8e30cf3eb8fb2b38bfed8115932d264de05
gbenhaim <gbenhaim@redhat.com>
2023-07-31 15:47:11 +0300

    Build image for RHTAP pipelines
    The image will be used in RHTAP Tekton pipelines.
    I contains the required dependencies, so there is no need to install
    them during runtime.
    
    Signed-off-by: gbenhaim <gbenhaim@redhat.com>

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,9 @@
-Dummy Dockerfile
+FROM registry.access.redhat.com/ubi9/python-39:latest
+
+RUN pip install --upgrade pip 'setuptools<58' wheel && \
+    pip install --upgrade 'crc-bonfire>=4.10.4'
+
+RUN curl https://downloads-openshift-console.apps.stone-prd-rh01.pg1f.p1.openshiftapps.com/amd64/linux/oc.tar \
+    | tar -C /opt/app-root/bin/ -xvf - 
+
+COPY bin/* /opt/app-root/bin/
---
+++ bin/parse-snapshot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import json
+import os
+from typing import Mapping
+from textwrap import dedent
+
+
+def main() -> None:
+    snapshot_str = os.environ.get("SNAPSHOT")
+    if snapshot_str is None:
+        raise RuntimeError("SNAPSHOT environment variable wasn't declared or empty")
+    snapshot: Mapping = json.loads(snapshot_str)
+    components = snapshot.get("components")
+    if not components:
+        raise RuntimeError(f"No components found in SNAPSHOT: ${snapshot}")
+    if len(components) > 1:
+        raise RuntimeError(
+            f"Can't handle snapshot that has more than 1 component. Got SNAPSHOT: ${snapshot}"
+        )
+
+    container_image = components[0]["containerImage"].split("@sha256")[0]
+    tag = components[0]["source"]["git"]["revision"]
+
+    print(dedent(
+        f"""
+        export IMAGE={container_image} IMAGE_TAG={tag} GIT_COMMIT={tag}
+        """
+    ))
+
+
+if __name__ == "__main__":
+    main()
