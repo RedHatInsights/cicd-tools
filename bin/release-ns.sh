82e0da6592c72f44da11e1859db1376637f07645
gbenhaim <gbenhaim@redhat.com>
2023-08-03 14:09:29 +0300

    add ns requester
    Signed-off-by: gbenhaim <gbenhaim@redhat.com>

--- bin/release-ns.sh
+++ bin/release-ns.sh
@@ -2,6 +2,11 @@
 
 main() {
     local ns_file="${1:?Namespace file was not provided}"
+    local ns_requester="${2:?Namespace requester name was not provided}"
+
+    export BONFIRE_NS_REQUESTER="$ns_requester"
+    export BONFIRE_BOT="true"
+    
     bonfire namespace release "$(cat "$ns_file")" -f
 }
 
--- bin/reserve-ns.sh
+++ bin/reserve-ns.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -ex
 
 main() {
-    local ns_file=${1:?A Path to a namespace file was not provided}
-    local namespace_pool="${2:-default}"
+    local ns_file="${1:?A Path to a namespace file was not provided}"
+    local ns_requester="${2:?Namespace requester name was not provided}"
+    local namespace_pool="${3:-default}"
+
+    export BONFIRE_BOT="true"
+    export BONFIRE_NS_REQUESTER="$ns_requester"
+
     bonfire namespace reserve --pool "$namespace_pool" > "$ns_file"
 }
 
