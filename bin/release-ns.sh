dbd3a2568e302a719f912d55dd5c42d9d65a5ff5
gbenhaim <gbenhaim@redhat.com>
2023-08-03 14:47:29 +0300

    fixes
    Signed-off-by: gbenhaim <gbenhaim@redhat.com>

--- bin/release-ns.sh
+++ bin/release-ns.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -ex
 
 main() {
-    local ns_file="${1:?Namespace file was not provided}"
+    local ns="${1:?Namespace was not provided}"
     local ns_requester="${2:?Namespace requester name was not provided}"
 
     export BONFIRE_NS_REQUESTER="$ns_requester"
     export BONFIRE_BOT="true"
-    
-    bonfire namespace release "$(cat "$ns_file")" -f
+
+    bonfire namespace release "$ns" -f
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
