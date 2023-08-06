cb06e16191d8ccacbbab5072cc9e5ce16d54b74f
gbenhaim <gbenhaim@redhat.com>
2023-08-06 15:19:21 +0300

    remove new line
    Signed-off-by: gbenhaim <gbenhaim@redhat.com>

--- bin/reserve-ns.sh
+++ bin/reserve-ns.sh
@@ -4,10 +4,12 @@ main() {
     local ns_file="${1:?A Path to a namespace file was not provided}"
     local ns_requester="${2:?Namespace requester name was not provided}"
     local namespace_pool="${3:-default}"
+    local ns
 
     export BONFIRE_NS_REQUESTER="$ns_requester"
 
-    bonfire namespace reserve --pool "$namespace_pool" > "$ns_file"
+    ns="$(bonfire namespace reserve --pool "$namespace_pool")" 
+    echo "${ns//'\n'}" > "$ns_file"
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
