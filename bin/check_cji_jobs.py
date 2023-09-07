1f11b18af15f9f95a7807bd9d35b1db415c0dc8f
gbenhaim <gbenhaim@redhat.com>
2023-09-07 10:12:23 +0300

    Simplify reading from stdin
    Signed-off-by: gbenhaim <gbenhaim@redhat.com>

--- bin/check_cji_jobs.py
+++ bin/check_cji_jobs.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
 import json
-import select
 import sys
 from textwrap import dedent
 
 
 def main() -> None:
-    rlist, _, _ = select.select([sys.stdin], [], [], 0)
-    if not rlist:
-        raise RuntimeError("No was provided to STDIN")
-    cji = json.load(rlist[0])
+    cji = json.load(sys.stdin)
     job_map: dict = cji["status"]["jobMap"]
     if not all(v == "Complete" for v in job_map.values()):
         print(dedent(
@@ -22,7 +18,7 @@ def main() -> None:
 
     print(dedent(
         f"""
-            All jobs completed: {job_map}   
+            All jobs succeeded: {job_map}   
         """
     ))
 
