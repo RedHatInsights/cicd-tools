83e5faf8ec17d0e1e721328aa754ad6b4ae75536
Matt Knop <mknop@redhat.com>
2023-10-12 15:10:20 -0600

    change requests

--- frontends/frontend-build.sh
+++ frontends/frontend-build.sh
@@ -4,10 +4,6 @@ set -x
 
 QUAY_EXPIRE_TIME="${IMAGE_AUTO_DELETION_DAYS}d"
 
-# source "./helpers/general.sh"
-
-# trap_proxy teardown EXIT ERR SIGINT SIGTERM
-
 NODE_BUILD_VERSION=`node -e 'console.log(require("./package.json").engines.node.match(/(\d+)\.\d+\.\d+/)[1])'`
 
 IMAGE="$APP_IMAGE"
@@ -15,7 +11,3 @@ IMAGE_TAG="${ghprbActualCommit:0:7}"
 
 curl -sSL "${COMMON_BUILDER}/src/frontend-build.sh" > ".frontend-build.sh"
 source "./.frontend-build.sh"
-
-BUILD_RESULTS=$?
-
-exit $BUILD_RESULTS
