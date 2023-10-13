0325459d0b1cd982a33e824bf7b041a7e7d8f1a8
Matt Knop <mknop@redhat.com>
2023-10-13 08:17:31 -0600

    removed dummy junit, cleanup

--- frontends/iqe_tests.sh
+++ frontends/iqe_tests.sh
@@ -15,14 +15,3 @@ source $CICD_ROOT/deploy_ephemeral_env.sh
 
 COMPONENT_NAME="$CJI_COMPONENT_NAME"
 source $CICD_ROOT/cji_smoke_test.sh
-
-RESULT=$?
-
-mkdir -p $WORKSPACE/artifacts
-cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF
-
-exit $RESULT
