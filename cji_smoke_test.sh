5c92783c67c4bb26f2c10127a7f87810caf88ac4
Matt Holder <mjholder96@gmail.com>
2023-10-25 11:36:39 -0400

    dont copy for testing bad path

--- cji_smoke_test.sh
+++ cji_smoke_test.sh
@@ -156,7 +156,7 @@ run_mc () {
     set +e
     docker run -t --net=host --name=$CONTAINER_NAME --entrypoint="/bin/sh" $MC_IMAGE -c "$CMD"
     RET_CODE=$?
-    docker cp $CONTAINER_NAME:/artifacts/. $ARTIFACTS_DIR
+    #docker cp $CONTAINER_NAME:/artifacts/. $ARTIFACTS_DIR
     docker rm $CONTAINER_NAME
     set -e
     return $RET_CODE
