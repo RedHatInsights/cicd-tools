344ecf2929831c31d4470157d84f57ad68743643
Victor M <vmugicag@redhat.com>
2024-01-10 12:08:27 +0100

    Add login to Openshift cluster logic (#45)
    * Add login to Openshift cluster logic
    
    * cleanup
    
    * set crcd as primary

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,31 +26,24 @@ export BONFIRE_REPO_ORG="${BONFIRE_REPO_ORG:-RedHatInsights}"
 
 set -x
 # Set up docker cfg
-export DOCKER_CONFIG="$WORKSPACE/.docker"
-rm -fr $DOCKER_CONFIG
-mkdir $DOCKER_CONFIG
-
-# Set up podman cfg
-# No longer needed due to podman now using the DOCKER_CONFIG
-#AUTH_CONF_DIR="$WORKSPACE/.podman"
-#rm -fr $AUTH_CONF_DIR
-#mkdir $AUTH_CONF_DIR
-#export REGISTRY_AUTH_FILE="$AUTH_CONF_DIR/auth.json"
+export DOCKER_CONFIG="${WORKSPACE}/.docker"
+rm -rf "$DOCKER_CONFIG"
+mkdir "$DOCKER_CONFIG"
 
 # Set up kube cfg
-export KUBECONFIG_DIR="$WORKSPACE/.kube"
-export KUBECONFIG="$KUBECONFIG_DIR/config"
-rm -fr $KUBECONFIG_DIR
-mkdir $KUBECONFIG_DIR
+export KUBECONFIG_DIR="${WORKSPACE}/.kube"
+export KUBECONFIG="${KUBECONFIG_DIR}/config"
+rm -rf "$KUBECONFIG_DIR"
+mkdir "$KUBECONFIG_DIR"
 
 set +x
 
 # if this is a PR, use a different tag, since PR tags expire
-if [ ! -z "$ghprbPullId" ]; then
+if [ -n "$ghprbPullId" ]; then
   export IMAGE_TAG="pr-${ghprbPullId}-${IMAGE_TAG}"
 fi
 
-if [ ! -z "$gitlabMergeRequestIid" ]; then
+if [ -n "$gitlabMergeRequestIid" ]; then
   export IMAGE_TAG="pr-${gitlabMergeRequestIid}-${IMAGE_TAG}"
 fi
 
@@ -58,7 +51,7 @@ fi
 export GIT_COMMIT=$(git rev-parse HEAD)
 export ARTIFACTS_DIR="$WORKSPACE/artifacts"
 
-rm -fr $ARTIFACTS_DIR && mkdir -p $ARTIFACTS_DIR
+rm -rf "$ARTIFACTS_DIR" && mkdir -p "$ARTIFACTS_DIR"
 
 # TODO: create custom jenkins agent image that has a lot of this stuff pre-installed
 export LANG=en_US.utf-8
@@ -71,12 +64,12 @@ python3 -m pip install --upgrade pip 'setuptools<58' wheel
 python3 -m pip install --upgrade 'crc-bonfire>=4.10.4'
 
 # clone repo to download cicd scripts
-rm -fr $BONFIRE_ROOT
+rm -rf "$BONFIRE_ROOT"
 echo "Fetching branch '$BONFIRE_REPO_BRANCH' of https://github.com/${BONFIRE_REPO_ORG}/cicd-tools.git"
 git clone --branch "$BONFIRE_REPO_BRANCH" "https://github.com/${BONFIRE_REPO_ORG}/cicd-tools.git" "$BONFIRE_ROOT"
 
 # Do a docker login to ensure our later 'docker pull' calls have an auth file created
-source ${CICD_ROOT}/_common_container_logic.sh
+source "${CICD_ROOT}/_common_container_logic.sh"
 login
 
 # Gives access to helper commands such as "oc_wrapper"
@@ -84,7 +77,66 @@ add_cicd_bin_to_path() {
   if ! command -v oc_wrapper; then export PATH=$PATH:${CICD_ROOT}/bin; fi
 }
 
-add_cicd_bin_to_path
+try_login_openshift_cluster() {
 
-echo "logging in to CRCD cluster"
-oc_wrapper login --token=$OC_LOGIN_TOKEN_DEV --server=$OC_LOGIN_SERVER_DEV
+    local success cluster_url cluster_token cluster_id
+
+    success=1
+
+    for cluster_id in crcd ephemeral ; do
+
+        cluster_url=''
+        cluster_token=''
+
+        _try_set_cluster_environment_variables "$cluster_id"
+
+        if [[ -z "$cluster_url" ]] || [[ -z "$cluster_token" ]]; then
+            echo "Environment variables for cluster '$cluster_id' not found"
+        else 
+            if _try_login_cluster "$cluster_url" "$cluster_token"; then
+                echo "Logged in to cluster '$cluster_id'"
+                success=0
+                break
+            else
+                echo "Failed logging into cluster: '$cluster_id'"
+            fi
+        fi
+    done
+
+    return "$success"
+}
+
+_try_set_cluster_environment_variables() {
+
+    local cluster_id="$1"
+
+    case "$cluster_id" in
+
+        crcd)
+            cluster_url="$OC_LOGIN_SERVER_DEV"
+            cluster_token="$OC_LOGIN_TOKEN_DEV"
+            ;;
+
+        ephemeral)
+            cluster_url="$OC_LOGIN_SERVER"
+            cluster_token="$OC_LOGIN_TOKEN"
+            ;;
+
+        *)
+            echo "Unknown cluster $cluster_id"
+            return 1
+            ;;
+    esac
+}
+
+_try_login_cluster() {
+    local url="$1"
+    local token="$2"
+    oc_wrapper login --token="$token" --server="$url"
+}
+
+add_cicd_bin_to_path
+if ! try_login_openshift_cluster; then
+    echo "Failed logging into any cluster!"
+    exit 1
+fi
