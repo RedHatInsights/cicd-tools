53d8ac226f8ef6c3dc13f7924a61b32daae8edbf
Matt Knop <mknop@redhat.com>
2024-03-11 14:22:12 -0600

    refactoring script

--- featureflags/unleash_config_export_import.sh
+++ featureflags/unleash_config_export_import.sh
@@ -1,83 +1,60 @@
-#!/bin/sh
-
-set -x
-
-# Pass in the name of the ephemeral namespace that will receive the imported toggles
-export EPHEMERAL_NAMESPACE="$1"
-
-###########################################################################################
-# In order to run this script, the following needs needs to be provided
-#
-# These will get removed from script when we convert to using a Kubernetes job
-###########################################################################################
-
-# EXPORT
-STAGE_UNLEASH_URL=""
-STAGE_UNLEASH_ADMIN_API_TOKEN=""          # This is retrieved in a later step until we convert to a job
-STAGE_UNLEASH_NAMESPACE=""
-STAGE_UNLEASH_SECRET_NAME=""              # This is retrieved in a later step until we convert to a job
-
-# IMPORT
-EE_UNLEASH_URL=""
-EE_UNLEASH_PERSONAL_ACCESS_TOKEN=""
+#!/bin/bash
 
 
-echo "\n
-###########################################################################################
-# Get name of FF pod from the ephemeral envionment
-###########################################################################################
-"
-EPHEMERAL_FF_POD=$(oc get pods -n $STAGE_UNLEASH_NAMESPACE | awk '/env-ephemeral/ && /featureflags/' | awk '{print $1}')
+EPHEMERAL_NAMESPACE="${EPHEMERAL_NAMESPACE:-}"
+EXPORT_UNLEASH_URL="${EXPORT_UNLEASH_URL:-}"
+EXPORT_NAMESPACE="${EXPORT_NAMESPACE:-}"
 
+ # This is retrieved in a later step until we convert to a job
+EXPORT_ADMIN_SECRET="${EXPORT_ADMIN_SECRET:-}"
+IMPORT_PAT="${IMPORT_PAT:-}"
 
-echo "\n
-###########################################################################################
-# Retrieve admin API token from namespace secrets in Stage cluster
-###########################################################################################
-"
-STAGE_FF_SECRET_NAME=$(oc get secrets -n $STAGE_UNLEASH_NAMESPACE | awk '/env-ephemeral/ && /featureflags/' | awk '{print $1}')
-export STAGE_UNLEASH_ADMIN_API_TOKEN=$(oc get secret $STAGE_FF_SECRET_NAME -n $STAGE_UNLEASH_NAMESPACE -o jsonpath='{.data.adminAccessToken}' | base64 --decode)
+declare -A required_env_vars
+required_env_vars["EPHEMERAL_NAMESPACE"]=$EPHEMERAL_NAMESPACE
+required_env_vars["EXPORT_UNLEASH_URL"]=$EXPORT_UNLEASH_URL
+required_env_vars["EXPORT_NAMESPACE"]=$EXPORT_NAMESPACE
+required_env_vars["EXPORT_ADMIN_SECRET"]=$EXPORT_ADMIN_SECRET
+required_env_vars["IMPORT_PAT"]=$IMPORT_PAT
 
+for var in "${required_env_vars[@]}"
+do
+  if {{ -z required_env_vars[var] }}; then
+    printf "variable [%s] was not set" "var"
+    return 1
+  fi
+done
 
-echo "\n
-###########################################################################################
-# Retrieve a list of toggle names
-###########################################################################################
-"
-curl -L -X GET "${STAGE_UNLEASH_URL}/api/admin/projects/default/features" \
+mkdir featureflags
+
+# Retrieve the admin api token from the export environment
+EXPORT_SECRET=$(oc get secrets -n $EXPORT_NAMESPACE | awk '/env-ephemeral/ && /featureflags/' | awk '{print $1}')
+EXPORT_ADMIN_SECRET=$(oc get secret $EXPORT_SECRET -n $EXPORT_NAMESPACE -o jsonpath='{.data.adminAccessToken}' | base64 --decode)
+
+# Retrieve a list of toggle names to export from environment
+curl -L -X GET "${EXPORT_UNLEASH_URL}/api/admin/projects/default/features" \
 -H "Accept: application/json" \
--H "Authorization: ${STAGE_UNLEASH_ADMIN_API_TOKEN}" > feature_flags_toggle_names.json
+-H "Authorization: ${EXPORT_ADMIN_SECRET}" > featureflags/feature_flags_toggle_names.json
 
-UNLEASH_PROJECT_TOGGLE_NAMES=$(jq -r [.features[].name] feature_flags_toggle_names.json)
+UNLEASH_PROJECT_TOGGLE_NAMES=$(jq -r [.features[].name] featureflags/feature_flags_toggle_names.json)
 echo $UNLEASH_PROJECT_TOGGLE_NAMES
 
-
-echo "\n
-###########################################################################################
-# Export toggles from Unleash pod in Stage cluster
-###########################################################################################
-"
-curl -L -X POST "${STAGE_UNLEASH_URL}/api/admin/features-batch/export" \
+# Export toggles
+curl -L -X POST "${EXPORT_UNLEASH_URL}/api/admin/features-batch/export" \
 -H "Content-Type: application/json" \
 -H "Accept: application/json" \
--H "Authorization: $STAGE_UNLEASH_ADMIN_API_TOKEN" \
+-H "Authorization: $EXPORT_ADMIN_SECRET" \
 --data-raw '{
   "environment": "development",
   "downloadFile": true,
   "features": '"${UNLEASH_PROJECT_TOGGLE_NAMES}"'
-}' > feature_flags_exported_toggles.json
+}' > featureflags/feature_flags_exported_toggles.json
 
-
-echo "\n
-###########################################################################################
-# Import toggles in to ephemeral environment
-###########################################################################################
-"
-EXPORTED_UNLEASH_TOGGLES=$(cat feature_flags_exported_toggles.json)
+# Import toggles into ephemeral environment
+EXPORTED_UNLEASH_TOGGLES=$(cat tmp/feature_flags_exported_toggles.json)
 
 curl -L -X POST  'http://localhost:4243/api/admin/features-batch/import' -H   'Content-Type: application/json' \
 -H 'Accept: application/json' \
--H "Authorization: ${EE_UNLEASH_PERSONAL_ACCESS_TOKEN}" \
+-H "Authorization: ${IMPORT_PAT}" \
 --data-raw '{
   "project": "default",
   "environment": "development",
@@ -89,4 +66,5 @@ echo "\n
 # Cleanup json files
 ###########################################################################################
 "
-find . -name \*.json -type f -delete
+
+rm -rf featureflags
