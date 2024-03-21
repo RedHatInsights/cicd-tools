e73e2a85a2f366f820badce86760505cc9fbb600
Matt Knop <mknop@redhat.com>
2024-03-21 12:16:14 -0600

    cleanup script

--- featureflags/unleash_config_export_import.sh
+++ featureflags/unleash_config_export_import.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-# set -x
+set -x
 
-function export_toggles() {
-    echo -e "\nRetrieve a list of toggle names to export from environment"
+echo "setting required environment variables"
+EPHEMERAL_NAMESPACE="${EPHEMERAL_NAMESPACE:-}"
+EXPORT_UNLEASH_URL="${EXPORT_UNLEASH_URL:-}"
+EXPORT_NAMESPACE="${EXPORT_NAMESPACE:-}"
+EXPORT_ADMIN_SECRET="${EXPORT_ADMIN_SECRET:-}"
+IMPORT_PAT="${IMPORT_PAT:-}"
+IMPORT_LOCALLY="${IMPORT_LOCALLY:-false}"
+JSON_TOGGLES_PATH="${JSON_TOGGLES_PATH:-featureflags/feature_flags_exported_toggles.json}"
+
+echo "JSON_TOGGLES_PATH: $JSON_TOGGLES_PATH"
+echo "EXPORT_UNLEASH_URL: $EXPORT_UNLEASH_URL"
+echo "IMPORT_LOCALLY: $IMPORT_LOCALLY"
+
+export_toggles() {
+    echo "Retrieve a list of toggle names to export from environment"
     curl -L -X GET "${EXPORT_UNLEASH_URL}/api/admin/projects/default/features" \
     -H "Accept: application/json" \
-    -H "Authorization: ${EXPORT_ADMIN_SECRET}" > featureflags/feature_flags_toggle_names.json
+    -H "Authorization: $EXPORT_ADMIN_SECRET" > "featureflags/feature_flags_toggle_names.json"
 
-    printf "JSON_TOGGLES_PATH: %s" "${JSON_TOGGLES_PATH}"
+    echo "JSON_TOGGLES_PATH: $JSON_TOGGLES_PATH"
     UNLEASH_PROJECT_TOGGLE_NAMES=$(jq -r [.features[].name] featureflags/feature_flags_toggle_names.json)
-    printf "\nlist of toggle names:\n%s\n" "$UNLEASH_PROJECT_TOGGLE_NAMES"
+    echo "list of toggle names: $UNLEASH_PROJECT_TOGGLE_NAMES"
 
-    echo -e "\nExporting toggles from environment"
+    echo -e "Exporting toggles from environment"
     curl -L -X POST "${EXPORT_UNLEASH_URL}/api/admin/features-batch/export" \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
@@ -21,60 +34,53 @@ function export_toggles() {
       "environment": "development",
       "downloadFile": true,
       "features": '"${UNLEASH_PROJECT_TOGGLE_NAMES}"'
-    }' > featureflags/feature_flags_exported_toggles.json
+    }' > "$JSON_TOGGLES_PATH"
 }
 
-function validate_environment_variables() {
-    echo -e "\nValidating that environment variables are set"
+validate_environment_variables() {
+    echo "Validating that environment variables are set"
     if [ -z "${EXPORT_UNLEASH_URL}" ]; then
-        echo -e "\nenvironment variable [EPHEMERAL_NAMESPACE] was not set"
-        return 1
-    fi
-
-    if [ -z "${EPHEMERAL_NAMESPACE}" ]; then
-        echo -e "\nenvironment variable [EXPORT_UNLEASH_URL] was not set"
+        echo "environment variable [EXPORT_UNLEASH_URL] was not set"
         return 1
     fi
 
     if [ -z "${EXPORT_NAMESPACE}" ]; then
-        echo -e "\nenvironment variable [EXPORT_NAMESPACE] was not set"
+        echo "environment variable [EXPORT_NAMESPACE] was not set"
         return 1
     fi
 
     if [ -z "${EXPORT_ADMIN_SECRET}" ]; then
-        echo -e "\nenvironment variable [EXPORT_ADMIN_SECRET] was not set"
+        echo "environment variable [EXPORT_ADMIN_SECRET] was not set"
+        return 1
+    fi
+
+    if [ -z "${EPHEMERAL_NAMESPACE}" ]; then
+        echo "environment variable [EPHEMERAL_NAMESPACE] was not set"
         return 1
     fi
 
     if [ -z "${IMPORT_PAT}" ]; then
-        echo -e "\nenvironment variable [IMPORT_PAT] was not set"
+        echo "environment variable [IMPORT_PAT] was not set"
         return 1
     fi
 
-    echo -e "\nAll required environment variables are set"
+    echo "\nAll required environment variables are set"
 }
 
-echo -e "\nsetting required environment variables"
-EPHEMERAL_NAMESPACE="${EPHEMERAL_NAMESPACE:-}"
-EXPORT_UNLEASH_URL="${EXPORT_UNLEASH_URL:-}"
-EXPORT_NAMESPACE="${EXPORT_NAMESPACE:-}"
-EXPORT_ADMIN_SECRET="${EXPORT_ADMIN_SECRET:-}"
-IMPORT_PAT="${IMPORT_PAT:-}"
-IMPORT_LOCALLY="${IMPORT_LOCALLY:-false}"
-JSON_TOGGLES_PATH="${JSON_TOGGLES_PATH:-'featureflags/feature_flags_exported_toggles.json'}"
+# if ! validate_environment_variables; then
+#     exit 1
+# fi
 
-validate_environment_variables
-
-echo -e "\nMaking directory for temp json files"
+echo "Making directory for temp json files"
 mkdir featureflags
 
 if [ "${IMPORT_LOCALLY}" == false ]; then
-    echo -e "\nenvironment variable [JSON_TOGGLES_PATH] was not set,\nexporting out of environment"
+    echo "environment variable [JSON_TOGGLES_PATH] was not set, exporting out of environment"
     export_toggles
 fi
 
 echo -e "\nImport toggles into ephemeral environment"
-EXPORTED_UNLEASH_TOGGLES=$(cat "featureflags/feature_flags_exported_toggles.json")
+EXPORTED_UNLEASH_TOGGLES=$(cat $JSON_TOGGLES_PATH)
 
 curl -L -X POST 'http://localhost:4243/api/admin/features-batch/import' -H 'Content-Type: application/json' \
 -H 'Accept: application/json' \
@@ -85,7 +91,7 @@ curl -L -X POST 'http://localhost:4243/api/admin/features-batch/import' -H 'Cont
   "data": '"$EXPORTED_UNLEASH_TOGGLES"'
 }'
 
-printf "\nToggles imported into ephemeral environment:\n%s\n" "$EXPORTED_UNLEASH_TOGGLES"
+echo "Toggles imported into ephemeral environment: $EXPORTED_UNLEASH_TOGGLES"
 
-echo -e "\nCleanup json files"
+echo "Cleanup json files"
 rm -rf featureflags
