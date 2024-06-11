1a364339379e509d89ebbc4d0d7e6dfb6ce44fbe
Victor M. <vmugicag@redhat.com>
2024-06-11 12:33:41 +0200

    fix(python): fixes python3 and pip3 installation

--- image_build_scripts/install_system_dependencies.sh
+++ image_build_scripts/install_system_dependencies.sh
@@ -2,7 +2,7 @@
 
 dependencies=(
     "python3.11"
-    "python3-pip"
+    "python3.11-pip"
     "shadow-utils"
     "tar"
     "gzip"
@@ -10,3 +10,6 @@ dependencies=(
 
 microdnf install -y "${dependencies[@]}"
 microdnf clean all
+
+alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.11 1
