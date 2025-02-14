5d663b42a6d466950f786125c568d8afbfe0973a
Vanessa Busch <vbusch@redhat.com>
2025-02-14 11:03:51 -0500

    fix(ci): update docker cp to only include the pwd contents
    `docker cp . ` isn't consistent.  Change it to docker cp ./. to copy the contents of the working directory without the directory itself.
    Signed-off-by: Vanessa Busch <vbusch@redhat.com>

--- frontends/lint.sh
+++ frontends/lint.sh
@@ -8,7 +8,7 @@ trap_proxy teardown EXIT ERR SIGINT SIGTERM
 
 docker run --name $TEST_CONT -d -i --rm $NODE_BASE_IMAGE /bin/sh
 
-docker cp . "${TEST_CONT}:/opt/app-root/src/"
+docker cp ./. "${TEST_CONT}:/opt/app-root/src/"
 
 docker exec -i -w "/opt/app-root/src/" $TEST_CONT sh -c "npm i"
 docker exec -i -w "/opt/app-root/src/" $TEST_CONT sh -c "npm run ci:lint"
