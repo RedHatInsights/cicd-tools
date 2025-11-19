602c88612b11f65956fd05cc791fc81fed172ee7
Ondřej Ezr <ezrik12@gmail.com>
2025-11-19 17:51:08 +0100

    feat(RHINENG-21824): Allow extending epehemral reservation duration (#164)
    In Konflux, we now allow extending the reservation duration time.
    This is needed for tests that do run over an hour.

--- konflux_scripts/reserve-ns.sh
+++ konflux_scripts/reserve-ns.sh
@@ -4,11 +4,12 @@ main() {
     local ns_file="${1:?A Path to a namespace file was not provided}"
     local ns_requester="${2:?Namespace requester name was not provided}"
     local namespace_pool="${3:-default}"
+    local reserve_duration=${RESERVE_DURATION:-1h}
     local ns
 
     export BONFIRE_NS_REQUESTER="$ns_requester"
 
-    ns="$(bonfire namespace reserve --pool "$namespace_pool")" 
+    ns="$(bonfire namespace reserve --pool "$namespace_pool" --duration ${reserve_duration})" 
     echo -n "${ns//'\n'}" > "$ns_file"
 }
 
