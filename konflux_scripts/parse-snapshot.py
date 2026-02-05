3a26eedc4ecc3d6e1c98183848854f56f2179e87
Juan Díaz <juandiazsuarez0@gmail.com>
2026-02-05 17:27:34 +0100

    [CCXDEV-15925] feat: allow mapping between one konflux component and multiple app-interface components (#184)
    * feat: allow mapping between one konflux component and multiple app-interface components
    
    * refactor: change function name

--- konflux_scripts/parse-snapshot.py
+++ konflux_scripts/parse-snapshot.py
@@ -1,3 +1,96 @@
+"""You can test this script with:
+
+A. Single component mapping:
+
+export SNAPSHOT='{
+    "application": "my-app",
+    "components": [
+    {
+        "name": "konflux-component1",
+        "containerImage": "quay.io/org/img1@sha256:abc123",
+        "source": {
+            "git": {
+                "url": "https://git.example.com/repo1",
+                "revision": "v1.0"
+            }
+        }
+    },
+    {
+        "name": "konflux-component2",
+        "containerImage": "quay.io/org/img2@sha256:abc456",
+        "source": {
+            "git": {
+                "url": "https://git.example.com/repo2",
+                "revision": "v2.0"
+            }
+        }
+    }
+    ]
+}'
+export BONFIRE_COMPONENTS_MAPPING='{"konflux-component1": "app-interface-component1"}'
+python parse-snapshot.py
+
+B. Multiple component mapping:
+
+export SNAPSHOT='{
+    "application": "my-app",
+    "components": [
+    {
+        "name": "konflux-component1",
+        "containerImage": "quay.io/org/img1@sha256:abc123",
+        "source": {
+            "git": {
+                "url": "https://git.example.com/repo1",
+                "revision": "v1.0"
+            }
+        }
+    },
+    {
+        "name": "konflux-component2",
+        "containerImage": "quay.io/org/img2@sha256:abc456",
+        "source": {
+            "git": {
+                "url": "https://git.example.com/repo2",
+                "revision": "v2.0"
+            }
+        }
+    }
+    ]
+}'
+export BONFIRE_COMPONENTS_MAPPING='{"konflux-component1": "app-interface-component1", "konflux-component2": "app-interface-component2"}'
+python parse-snapshot.py
+
+C. Konflux component mapping with multiple app-interface components:
+
+export SNAPSHOT='{
+    "application": "my-app",
+    "components": [
+    {
+        "name": "konflux-component1",
+        "containerImage": "quay.io/org/img1@sha256:abc123",
+        "source": {
+            "git": {
+                "url": "https://git.example.com/repo1",
+                "revision": "v1.0"
+            }
+        }
+    },
+    {
+        "name": "konflux-component2",
+        "containerImage": "quay.io/org/img2@sha256:abc456",
+        "source": {
+            "git": {
+                "url": "https://git.example.com/repo2",
+                "revision": "v2.0"
+            }
+        }
+    }
+    ]
+}'
+export BONFIRE_COMPONENTS_MAPPING='{"konflux-component1": ["app-interface-component1", "app-interface-component2"]}'
+python parse-snapshot.py
+"""
+
 #!/usr/bin/env python3
 
 import json
@@ -53,7 +146,13 @@ class Snapshot(BaseModel):
 
 
 def parse_component_mapping(mapping_str):
-    """Parses json translation of Konflux component name into bonfire component"""
+    """Parses json translation of Konflux component name into bonfire component(s).
+
+    Values may be a single string or a list of strings (one Konflux component → many app-interface components).
+    For example:
+    - {'{"konflux_component1": "app_interface_component1", "konflux_component2": "app_interface_component2"}'}
+    - {'{"konflux_component1": "app_interface_component1", "konflux_component2": ["app_interface_component2", "app_interface_component3"]}'}
+    """
     mapping = {}
     if mapping_str:
          mapping = json.loads(mapping_str)
@@ -74,21 +173,29 @@ def main() -> None:
         # check if the snapshot component name has a mapping defined in BONFIRE_COMPONENTS
         # ... if not, check if BONFIRE_COMPONENT_NAME is set
         # ... if not, just use the snapshot component name
-        component_name = component_mapping.get(
+        component_names: str | list[str] = component_mapping.get(
             snapshot_component.name,
             bonfire_component_name or snapshot_component.name
         )
 
-        ret.extend((
-            "--set-template-ref",
-            f"{component_name}={snapshot_component.source.git.revision}",
-            "--set-parameter",
-            f"{component_name}/IMAGE={snapshot_component.container_image.image}@sha256",
-            "--set-parameter",
-            f"{component_name}/IMAGE_TAG={snapshot_component.container_image.sha}"
-        ))
+        if isinstance(component_names, str):
+            _add_component_to_params(ret, component_names, snapshot_component)
+        elif isinstance(component_names, list):
+            for component_name in component_names:
+                _add_component_to_params(ret, component_name, snapshot_component)
+        else:
+            raise ValueError(f"Invalid component mapping: {component_mapping}. Expected a string or a list of strings.")
     print(" ".join(ret))
 
+def _add_component_to_params(params: list[str], component_name: str, snapshot_component: Component) -> None:
+    params.extend((
+        "--set-template-ref",
+        f"{component_name}={snapshot_component.source.git.revision}",
+        "--set-parameter",
+        f"{component_name}/IMAGE={snapshot_component.container_image.image}@sha256",
+        "--set-parameter",
+        f"{component_name}/IMAGE_TAG={snapshot_component.container_image.sha}"
+    ))
 
 if __name__ == "__main__":
     main()
